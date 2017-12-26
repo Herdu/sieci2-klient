69fe90e2e128ac01958275e9ee3d3248d89ddca5
Mateusz <mateusz.dobrowolski71@gmail.com>
2017-12-26 15:46:14 +0100

    new makefile for server. Testing

--- server/server.cpp
+++ server/server.cpp
@@ -1,70 +1,212 @@
-#include <iostream>
-#include <fcntl.h>
+#include <cstdlib>
+#include <cstdio>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <cstdio>
-#include <cstdlib>
+#include <errno.h>
+#include <error.h>
+#include <netdb.h>
+#include <sys/epoll.h>
+#include <poll.h>
+#include <thread>
+#include <unordered_set>
+#include <signal.h>
+#include <sys/epoll.h>
+#include <sys/timerfd.h>
+#include <iostream>
+#include <strings.h>
+#include <string.h>
+
+
+#include "src/GameTime.h"
 
 using namespace std;
-int main(int argc, char **argv) {
-    sockaddr_in addr;
-    addr.sin_family=AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    uint16_t hostPort =1333;
-    uint16_t hostportReverse = htons(hostPort);
-    addr.sin_port = hostportReverse;
-    int fd, nfd;
 
-    if(((fd=socket(AF_INET, SOCK_STREAM,0))==-1)){
-        perror("Blad otworzenie socketu\n");
-        exit(1);
+GameTime gameTime;
+
+// server socket
+int servFd;
+
+// client sockets
+std::unordered_set<int> clientFds;
+
+// handles SIGINT
+void ctrl_c(int);
+
+// sends data to clientFds excluding fd
+void sendToAllBut(int fd, char * buffer, int count);
+
+// converts cstring to port
+uint16_t readPort(char * txt);
+
+// sets SO_REUSEADDR
+void setReuseAddr(int sock);
+
+void acceptConnection(int epollHandler){
+    // prepare placeholders for client address
+    sockaddr_in clientAddr{0};
+    socklen_t clientAddrSize = sizeof(clientAddr);
+
+    // accept new connection
+    auto clientFd = accept(servFd, (sockaddr*) &clientAddr, &clientAddrSize);
+    if(clientFd == -1) error(1, errno, "accept failed");
+
+    // add client to all clients set
+    clientFds.insert(clientFd);
+
+    // tell who has connected
+    printf("new connection from: %s:%hu (fd: %d)\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port), clientFd);
+
+    epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = clientFd;
+
+    epoll_ctl(epollHandler, EPOLL_CTL_ADD, clientFd, &event);
+}
+
+void sendMessage(int clientFd){
+    // read a message
+    char buffer[255];
+    int count = read(clientFd, buffer, 255);
+
+    if(count < 1) {
+        printf("removing %d\n", clientFd);
+        clientFds.erase(clientFd);
+        close(clientFd);
+    } else {
+        // broadcast the message
+        sendToAllBut(clientFd, buffer, count);
     }
+}
+
+
+int main(int argc, char ** argv){
+    // get and validate port number
+    //if(argc != 2) error(1, 0, "Need 1 arg (port)");
+    auto port = readPort("12345"/*argv[1]*/);
+
+
+    /********************************timer part */
+    int tfd;//timer fd
+    if((tfd= timerfd_create(CLOCK_MONOTONIC,TFD_NONBLOCK)) < 0)
+        cout<<"timerfd create error"<<endl;
+    struct itimerspec newValue;
+    struct itimerspec oldValue;
+    bzero(&newValue,sizeof(newValue));
+    bzero(&oldValue,sizeof(oldValue));
+    struct timespec ts;
+    ts.tv_sec = 5;
+    ts.tv_nsec = 0;
+    //both interval and value have been set
+    newValue.it_value = ts;
+    newValue.it_interval = ts;
+    if( timerfd_settime(tfd,0,&newValue,&oldValue) <0)
+    {
+        cout<<"settime error"<<strerror(errno)<<endl;
+    }
+
+
+
+
+    /********************************timer part END */
+
+
+    // create socket
+    servFd = socket(AF_INET, SOCK_STREAM, 0);
+    if(servFd == -1) error(1, errno, "socket failed");
+
+    // graceful ctrl+c exit
+    signal(SIGINT, ctrl_c);
+    // prevent dead sockets from throwing pipe errors on write
+    signal(SIGPIPE, SIG_IGN);
+
+    setReuseAddr(servFd);
+
+    // bind to any address and port provided in arguments
+    sockaddr_in serverAddr{.sin_family=AF_INET, .sin_port=htons((short)port), .sin_addr={INADDR_ANY}};
+    int res = bind(servFd, (sockaddr*) &serverAddr, sizeof(serverAddr));
+    if(res) error(1, errno, "bind failed");
+
+    // enter listening mode
+    res = listen(servFd, 1);
+    if(res) error(1, errno, "listen failed");
+
+/****************************/
+
+    int epollHandler = epoll_create1(0);
+
+    epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = servFd;
+
+    epoll_ctl(epollHandler, EPOLL_CTL_ADD, servFd, &event);
+
+
+    /*** time ***/
+
+    event.data.fd = tfd;
+    event.events = EPOLLIN | EPOLLET;
+
+    if( epoll_ctl(epollHandler,EPOLL_CTL_ADD,tfd,&event) < 0)
+        cout<<"epoll_ctl error"<<endl;
+
+    /******/
+
+
+
+    int resultCount;
+    while((resultCount = epoll_wait(epollHandler, &event, 1, -1)) != -1){
+        if( event.events == EPOLLIN && event.data.fd == servFd ){
+            acceptConnection(epollHandler);
+        }
+
+        if( event.events == EPOLLIN && event.data.fd != servFd && event.data.fd != tfd){
+            sendMessage(event.data.fd);
+        }
+
+        if( event.events == EPOLLIN && event.data.fd == tfd){
+            uint64_t value;
+            read(tfd, &value, 8);
+            cout<<value<<endl;
+        }
+
+    }
+}
+
+uint16_t readPort(char * txt){
+    char * ptr;
+    auto port = strtol(txt, &ptr, 10);
+    if(*ptr!=0 || port<1 || (port>((1<<16)-1))) error(1,0,"illegal argument %s", txt);
+    return port;
+}
+
+void setReuseAddr(int sock){
     const int one = 1;
-    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
-    if(bind(fd, (struct sockaddr*)&addr, sizeof(addr))==-1)
-    {
-        perror("problem zwiazania polaczenia - zajety port!\n");
-    };
-    if(listen(fd, 10)==-1)
-    {
-        perror("problem nasluchiwania\n");
-    };
-    socklen_t len;
-    char ipstr[INET_ADDRSTRLEN];
-    char buf[255] = "Witaj na serwerze!\n";
-    char buf2[255];
+    int res = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+    if(res) error(1,errno, "setsockopt failed");
+}
 
-    while(1) {
-        if ((nfd = accept(fd, (struct sockaddr *) &addr, &len)) == -1 ) {
-            perror("problem nawiazania polaczenia\n");
-        };
-	int size;
-	while(1)
-	{
-		if((size=read(nfd, buf2, sizeof(buf)-1))==-1)
-		{
-			perror("blad odczytu od klienta\n");
-		}
-		else
-		{
-			if(write(1, "Odebrano wiadomosc: ", 21)==-1)
-				perror("blad wypisania na ekran\n");
-		};
-		if(write(1, buf2,size)==-1)
-			perror("blad wypisania na ekran\n");
+void ctrl_c(int){
+    for(int clientFd : clientFds)
+        close(clientFd);
+    close(servFd);
+    printf("Closing server\n");
+    exit(0);
+}
 
-   		if(write(nfd, buf, sizeof(buf) - 1)==-1)
-			perror("blad wypisania na ekran\n");
-
-		if(write(1, "od ", 3)==-1)
-			perror("blad wypisania na ekran\n");
-		struct sockaddr_in *s = (struct sockaddr_in *) &addr;
-        	int port = ntohs(s->sin_port);
-        	inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
-        	printf("%s %d\n", ipstr, port);
-        };
-        shutdown(nfd, SHUT_RDWR);
+void sendToAllBut(int fd, char * buffer, int count){
+    int res;
+    decltype(clientFds) bad;
+    for(int clientFd : clientFds){
+        if(clientFd == fd) continue;
+        res = write(clientFd, buffer, count);
+        if(res!=count)
+            bad.insert(clientFd);
+    }
+    for(int clientFd : bad){
+        printf("removing %d\n", clientFd);
+        clientFds.erase(clientFd);
+        close(clientFd);
     }
 }
---
+++ server/src/GameTime.cpp
@@ -0,0 +1,12 @@
+//
+// Created by root on 23.12.17.
+//
+
+using namespace std;
+
+#include "GameTime.h"
+
+
+GameTime::GameTime() {
+    cout << "test" << endl;
+}
